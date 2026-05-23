4ba76a4174765d9888e3882ebd353705686c8ae1
sftnaila27-code <sftnaila27@gmail.com>
2026-05-23 01:33:37 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from io import StringIO, BytesIO
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
